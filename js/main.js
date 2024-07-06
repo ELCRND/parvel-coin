fb378a133aa95bd5fcc006520a65b4d8b9ec3e57
ELCRND <jwhh2a@gmail.com>
2024-07-06 08:27:34 +0300

    modified:   css/styles.css 	modified:   css/styles.css.map 	modified:   js/main.js 	modified:   scss/base/_base.scss 	modified:   scss/blocks/header.scss

--- js/main.js
+++ js/main.js
@@ -3,18 +3,23 @@ const menu = document.querySelector(".header__menu");
 const toggleBtns = document.querySelectorAll(".person__btn");
 const personsInfo = document.querySelectorAll(".person");
 
+// открыть/закрыть меню
+menuBtn.addEventListener("click", () => {
+  menu.classList.toggle("visible");
+  menuBtn.classList.toggle("close");
+  document.body.classList.toggle("scroll-off");
+});
+
+// закрытие меню при нажатии на ссылку
 document.querySelectorAll(".menu__link").forEach((link) =>
   link.addEventListener("click", () => {
     menu.classList.remove("visible");
     menuBtn.classList.remove("close");
+    document.body.classList.remove("scroll-off");
   })
 );
 
-menuBtn.addEventListener("click", () => {
-  menu.classList.toggle("visible");
-  menuBtn.classList.toggle("close");
-});
-
+// переключение контента в блоке about
 toggleBtns.forEach((btn) =>
   btn.addEventListener("click", (e) => {
     const contentType = e.target.parentNode.getAttribute("data-type");
